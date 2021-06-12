a83e8afec21095d8d07cff21ab05bcc7d9c10041
Tijn Roozen <tijnroozen@gmail.com>
2021-06-12 12:12:39 +0200

    some theme helpers added + extra setting up for Nav bar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import styled from 'styled-components'
+import { colors } from '../theme/colors';
+import { typography } from '../theme/typography';
 
 const Title = styled.h1`
-color: green;
+    color: ${props => props.color || colors.background};
+    font-size: ${typography.fontSizes.['6xl']};
+`
+
+const HeroPlaceHolder = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: ${colors.background};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const TextPlaceHolder = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-color: ${colors.white};
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 
 function Home() {
     return (
         <div>
-           <Title>Eén portfolio.</Title>
+           <HeroPlaceHolder><Title color={colors.white}>Hero PlaceHolder</Title></HeroPlaceHolder>
+           <TextPlaceHolder><Title>Text PlaceHolder</Title></TextPlaceHolder>
         </div>
     )
 }
---
+++ src/theme/container.js
@@ -0,0 +1,8 @@
+export const container = {
+    maxWidth: 1440,
+    maxWidthInner: 1280,
+    paddingH: 20,
+    paddingHMobile: 10,
+    paddingV: 80,
+    paddingVMobile: 80,
+}
