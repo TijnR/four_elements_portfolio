b23a5f153506102e6da644cfcd72fedcf8a56007
Tijn Roozen <tijnroozen@gmail.com>
2021-06-21 13:53:31 +0200

    small changes to listdata for carousel

--- src/listItems.js
+++ src/listItems.js
@@ -26,16 +26,16 @@ export const listProjects = [
     {name: "barry boogert", projectName: "Grafisch ontwerp", to: '/barry/grafisch-ontwerp', img: 1},
     {name: "justin van wijk", projectName: "Web - 3D", to: '/justin/web-3D', img: 4},
     {name: "kim van der waart", projectName: "Visual Identity", to: '/kim/visual-identity', img: 7},
-    {name: "tijn roozen", projectName: "react & typescript", to: '/tijn/react-typescript', img: 10},
+    {name: "tijn roozen", projectName: "React & Typescript", to: '/tijn/react-typescript', img: 10},
 
-    {name: "barry boogert", projectName: "Grafisch ontwerp", to: '/barry/grafisch-ontwerp', img: 2},
-    {name: "justin van wijk", projectName: "Web - 3D", to: '/justin/web-3D', img: 5},
-    {name: "kim van der waart", projectName: "Visual Identity", to: '/kim/visual-identity', img: 8},
-    {name: "tijn roozen", projectName: "react & typescript", to: '/tijn/react-typescript', img: 11},
+    {name: "barry boogert", projectName: "Webdesign", to: '/barry/grafisch-ontwerp', img: 2},
+    {name: "justin van wijk", projectName: "Phaser.js", to: '/justin/web-3D', img: 5},
+    {name: "kim van der waart", projectName: "Hand Lettering", to: '/kim/visual-identity', img: 8},
+    {name: "tijn roozen", projectName: "IOS Developement", to: '/tijn/react-typescript', img: 11},
 
-    {name: "barry boogert", projectName: "Grafisch ontwerp", to: '/barry/grafisch-ontwerp', img: 3},
-    {name: "justin van wijk", projectName: "Web - 3D", to: '/justin/web-3D', img: 6},
-    {name: "kim van der waart", projectName: "Visual Identity", to: '/kim/visual-identity', img: 9},
-    {name: "tijn roozen", projectName: "React & Typescript", to: '/tijn/react-typescript', img: 12},
+    {name: "barry boogert", projectName: "UX/UI", to: '/barry/grafisch-ontwerp', img: 3},
+    {name: "justin van wijk", projectName: "Automotive Design", to: '/justin/web-3D', img: 6},
+    {name: "kim van der waart", projectName: "Abstract Background Animation", to: '/kim/visual-identity', img: 9},
+    {name: "tijn roozen", projectName: "Three.js & React-Three-Fiber", to: '/tijn/react-typescript', img: 12},
 
 ]
