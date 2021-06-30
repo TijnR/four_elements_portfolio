bfd6c91113c4755e45ec22ea440faeaae87d0a21
Tijn Roozen <tijnroozen@gmail.com>
2021-06-30 20:28:09 +0200

    test again

--- src/components/molecules/HeroScrollButton.js
+++ src/components/molecules/HeroScrollButton.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React from 'react'
 import styled, {keyframes} from 'styled-components';
 import { colors } from '../../theme/colors';
 import { spacing } from '../../theme/spacing';
